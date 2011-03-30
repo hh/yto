017bf8d81f0a65524fb7bb32569b2b1ccc924ef8
Chris McCliamns <chris@hippiehacker.org>
2011-03-31 11:09:17 +1300

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yto::Application.config.secret_token = '6f22dbeb9ad69afc3b37e369052589d87819e9e6a565e3cf93aced1af279d9d432652bb6660c03de403f91a244346b2383dec89cfd2a8e6d5903ce34e9c9922e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Yto::Application.config.session_store :cookie_store, :key => '_yto_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Yto::Application.config.session_store :active_record_store
